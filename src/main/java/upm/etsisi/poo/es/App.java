b008ca8a9ffe6aee6137c23525b9de494d984e28
Miguel <miguel.aa0210@gmail.com>
2025-09-26 10:09:22 +0200

    Class Product and Ticket created

--- src/main/java/upm/etsisi/poo/es/App.java
+++ src/main/java/upm/etsisi/poo/es/App.java
@@ -4,6 +4,8 @@ package upm.etsisi.poo.es;
  * Hello world!
  *
  */
+
+
 public class App 
 {
     public static void main( String[] args )
---
+++ src/main/java/upm/etsisi/poo/es/Product.java
@@ -0,0 +1,11 @@
+package upm.etsisi.poo.es;
+
+enum type {
+  MERCH, PAPELERIA, ROPA, LIBRO, ELECTRONICA
+}
+
+public class Product {
+  private int Id;
+  private String name;
+
+}
