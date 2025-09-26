55b7db0f198163e90d136a45dc4b163d3d247038
AaronAAS06 <aaronapuntesierra@gmail.com+>
2025-09-26 11:57:56 +0200

    Clase ticket comenzada.

--- src/main/java/upm/etsisi/poo/es/Ticket.java
+++ src/main/java/upm/etsisi/poo/es/Ticket.java
@@ -1,4 +1,33 @@
 package upm.etsisi.poo.es;
 
+import java.util.ArrayList;
+
 public class Ticket {
+    int amount;
+    ArrayList<Product> productList;
+    private final int MAX_PRODUCT=100;
+
+    public Ticket(){
+        productList = new ArrayList<>();
+        amount= 0;
+    }
+
+    /**
+     * @param product
+     * Method that add the product to the list just when the amount is below 100.
+     * @return boolean
+     */
+    public boolean add(Product product){  //Metodo
+        boolean done = true;
+        if(amount<= 100){
+            productList.add(product.getId(),product);
+            amount++;
+        }
+        else done = false;
+        return done;
+    }
+    public boolean remove(Product product){
+        return false;
+    }
+
 }
