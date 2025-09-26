0a50082c47b75bbefc169afa8c7b59033225aff3
inaki <inakitaipeticse8@gmail,com>
2025-09-26 12:00:57 +0200

    Valor del precio para que el precio sea positivo siempre y que el nombre tenga mas de 100 caracteres y que no sea vacio

--- src/main/java/upm/etsisi/poo/es/Product.java
+++ src/main/java/upm/etsisi/poo/es/Product.java
@@ -30,12 +30,30 @@ public class Product {
     public void setId(int id) {
         Id = id;
     }
-
+    //El nombre del producto debe ser menos de 100 caracteres y no debe ser vacio.
     public void setName(String name) {
+      String[] nameComplete = name.split(" ");
+      int n=0;
+      if(nameComplete.length<100 && !name.isEmpty()){
         this.name = name;
+      }else{
+        if(!(nameComplete.length<100)){
+          System.out.println("The name should have less of 100 characters");
+        }
+        if(name.isEmpty()){
+          System.out.println("The name shouldn't be empty");
+        }
+      }
     }
-
+    //Obligar que el valor del precio sea siempre positivo
     public void setPrice(int price) {
+      if(price>0){
         this.price = price;
+      }else{
+        this.price = 1;
+        System.out.println("WARNING: The price should be a positive number");
+      }
+
+
     }
 }
