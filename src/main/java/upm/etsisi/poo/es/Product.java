ba2bf719d06000e3f1f4a55258993d63d6beff92
Miguel <miguel.aa0210@gmail.com>
2025-09-26 10:27:17 +0200

    Class Product getters and setters

--- src/main/java/upm/etsisi/poo/es/Product.java
+++ src/main/java/upm/etsisi/poo/es/Product.java
@@ -7,5 +7,35 @@ enum type {
 public class Product {
   private int Id;
   private String name;
+  private int price;
 
+    public Product(String name, int price, int id) {
+        this.name = name;
+        this.price = price;
+        Id = id;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
