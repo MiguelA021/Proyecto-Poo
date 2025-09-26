85306227d469bf0233cd8f5a65539c18f3e11967
Miguel <miguel.aa0210@gmail.com>
2025-09-26 12:22:26 +0200

    cuantity added to product

--- src/main/java/upm/etsisi/poo/es/Product.java
+++ src/main/java/upm/etsisi/poo/es/Product.java
@@ -8,52 +8,62 @@ public class Product {
   private int Id;
   private String name;
   private int price;
+  private int cuantity;
 
-    public Product(String name, int price, int id) {
-        this.name = name;
-        this.price = price;
-        Id = id;
-    }
+  public Product(String name, int price, int id, int cuantity) {
+    this.name = name;
+    this.price = price;
+    this.Id = id;
+    this.cuantity = cuantity;
+  }
 
-    public int getId() {
-        return Id;
-    }
+  public int getCuantity() {
+    return cuantity;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public int getId() {
+    return Id;
+  }
 
-    public int getPrice() {
-        return price;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setId(int id) {
-        Id = id;
-    }
-    //El nombre del producto debe ser menos de 100 caracteres y no debe ser vacio.
-    public void setName(String name) {
-      String[] nameComplete = name.split(" ");
-      int n=0;
-      if(nameComplete.length<100 && !name.isEmpty()){
-        this.name = name;
-      }else{
-        if(!(nameComplete.length<100)){
-          System.out.println("The name should have less of 100 characters");
-        }
-        if(name.isEmpty()){
-          System.out.println("The name shouldn't be empty");
-        }
+  public int getPrice() {
+    return price;
+  }
+
+  public void setCuantity(int cuantity) {
+    this.cuantity = cuantity;
+  }
+
+  public void setId(int id) {
+    this.Id = id;
+  }
+
+  // Name should be of less than 100 characters and nos empty
+  public void setName(String name) {
+    String[] nameComplete = name.split(" ");
+    if (name.length() < 100 && !name.isEmpty()) {
+      this.name = name;
+    } else {
+      if (!(name.length() < 100)) {
+        System.out.println("The name should have less than 100 characters");
+      }
+      if (name.isEmpty()) {
+        System.out.println("The name shouldn't be empty");
       }
     }
-    //Obligar que el valor del precio sea siempre positivo
-    public void setPrice(int price) {
-      if(price>0){
-        this.price = price;
-      }else{
-        this.price = 1;
-        System.out.println("WARNING: The price should be a positive number");
-      }
+  }
 
-
+  // Price is always positive
+  public void setPrice(int price) {
+    if (price > 0) {
+      this.price = price;
+    } else {
+      this.price = 1;
+      System.out.println("WARNING: The price should be a positive number");
     }
+
+  }
 }
