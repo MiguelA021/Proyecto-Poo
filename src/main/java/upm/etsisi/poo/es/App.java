3e7ab90dc8de57be82d02bd40493f21ab7c29cf2
PantojaAlejandro <alejandro.pantoja.figueroa@gmail.com>
2025-10-04 16:50:08 +0200

    Included the method readFile, also included new javadoc on the class App and the constants with some messages related with errors.

--- src/main/java/upm/etsisi/poo/es/App.java
+++ src/main/java/upm/etsisi/poo/es/App.java
@@ -1,61 +1,119 @@
 package upm.etsisi.poo.es;
-//BORAR ESTE COMENTARIO
-import java.lang.reflect.Type;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
-public class App
-{
-    public static void main( String[] args ) {
+public class App {// REVISAR: Se deberia hacer una parte static para inicializar los mensajes de errores
+    final static String FILE_ERROR = "Error while reading the file, please try again";
+    final static String FORMAT_ERROR = "Error with the format, please try again";
+    final static String DATA_ERROR  = "Error with the data, please introduce the data which is within restrictions";
+
+    public static void main(String[] args) {
         App app = new App();
         app.init();
-        app.start();
+        app.start(args);
         app.end();
-
     }
 
     private void end() {
     }
 
-    private void start() {
-        Scanner scan = new Scanner(System.in);
-        boolean end = false;
+    private void start(String[] args) {
+        if (args.length==0) {
+            readCommand();
+        } else {
+            readFile(args);
+        }
+    }
+
+    /**
+     * The method reads the commands through a file, given by the args.
+     * @param args the array holds the path of the file
+     */
+    private void readFile(String[] args){
+        String[] command;
         Ticket ticket = new Ticket();
         Store store = new Store();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            boolean end=false;
+            while(!end){
+                String line= reader.readLine();
+                if (line!=null){
+                    command=line.split(" ");
+                    end=readCommandAux(command, store, ticket, line);
+                } else {
+                    end=true;
+                    readCommand();
+                }
+            }
+            reader.close();
+        } catch (IOException e){
+            System.out.println(FILE_ERROR);
+        }
+    }
 
-
+    /**
+     * The method reads the commands given by the terminal
+     */
+    private void readCommand(){
+        boolean end = false;
+        Scanner scan = new Scanner(System.in);
+        Ticket ticket = new Ticket();
+        Store store = new Store();
         while(!end){
             String command = scan.nextLine();
             String [] commandArray = command.split(" ");
-            switch (commandArray[0]){
-                case "prod":
-                    commandProd(commandArray,store,ticket);
-                    break;
-                case "ticket":
-                    switch (commandArray[1]){
-                        case "new":
-                            ticket= new Ticket();
-                    }
-                    break;
-                case "help":
-                    printHelp();
-                    break;
-                case "echo":
-                    try {
-                        String[] commandMarks = command.split("\"");
-                        System.out.println(commandMarks[1]);
-                    }catch(ArrayIndexOutOfBoundsException e ){
-                        System.out.println("Incorrect format, try again.");
-                    }
-                    break;
-                case "exit":
-                    end = true;
-                    break;
-
-            }
+            end=readCommandAux(commandArray,store,ticket,command);
         }
-
     }
 
+    /**
+     * The method executes the methods of each command, given by command and commandArray
+     * @param commandArray is the command sliced
+     * @param store the array of products
+     * @param ticket
+     * @param command the command given by the user
+     * @return
+     */
+    private boolean readCommandAux(String[] commandArray, Store store, Ticket ticket, String command){
+        boolean end=false;
+        switch (commandArray[0]){
+            case "prod":
+                commandProd(commandArray,store,ticket);
+                break;
+            case "ticket":
+                switch (commandArray[1]){
+                    case "new":
+                        ticket= new Ticket();
+                }
+                break;
+            case "help":
+                printHelp();
+                break;
+            case "echo":
+                try {
+                    String[] commandMarks = command.split("\"");
+                    System.out.println(commandMarks[1]);
+                }catch(ArrayIndexOutOfBoundsException e ){
+                    System.out.println(FORMAT_ERROR);
+                }
+                break;
+            case "exit":
+                end = true;
+                break;
+
+        }
+        return end;
+    }
+
+    /**
+     * The method manages when the command given is related with prod
+     * @param commandArray is the command sliced by blanks
+     * @param store the array of products
+     * @param ticket
+     */
     private void commandProd(String[] commandArray,Store store, Ticket ticket) {
         switch (commandArray[1]) {
             case "add":
@@ -65,7 +123,7 @@ public class App
                     price = Double.parseDouble(commandArray[5]);
                     type type = upm.etsisi.poo.es.type.valueOf(commandArray[4]);
                 } catch (NumberFormatException e) {
-                    System.out.println("Introduce los datos correctos");
+                    System.out.println(DATA_ERROR);
                     correct = false;
                 }
                 if (correct) {
