bf69a119fce7ea93cbc62d5572e173b08803615a
inaki <inakitaipeticse8@gmail,com>
2025-10-11 01:49:48 +0200

    I add a new method toString for showing the discount
    If you see the guide pdf about this proyect, you'll see as we need to put
    the 'discountValue' next to the end of the product.toString when we print ticket
    so I create a new toString for ticket.print

--- src/main/java/upm/etsisi/poo/es/Product.java
+++ src/main/java/upm/etsisi/poo/es/Product.java
@@ -105,10 +105,11 @@ public class Product {
 
     @Override
     public String toString() {
-        double discountValue = this.price - this.getDiscountedPrice();
         return "{class:Product, id: " + this.id + ", name: '" + this.name + "', category: " + this.category + ", price: " + this.price + "}";
 
     }
-
-
+    public String toStringTicket(){
+        double discountValue = this.price - this.getDiscountedPrice();
+        return String.format("{class:Product, id: %d, name: '%s', category: %s, price: %.2f} **discount -%.2f", this.id, this.name, this.category, this.price, discountValue);
+    }
 }
