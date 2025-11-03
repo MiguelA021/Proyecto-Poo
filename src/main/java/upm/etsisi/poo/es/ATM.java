6c383becd8b53958fb397fcc646244db6a68d021
PantojaAlejandro <alejandro.pantoja.figueroa@gmail.com>
2025-11-03 18:20:04 +0100

    Added methods: listTickets() and removeTickets(). I also made minor changes

--- src/main/java/upm/etsisi/poo/es/ATM.java
+++ src/main/java/upm/etsisi/poo/es/ATM.java
@@ -1,22 +1,44 @@
 package upm.etsisi.poo.es;
-import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class ATM extends User {
-    private HashMap<Integer, Ticket> tickets;//Ver si cambiar a arboles B
+    private TreeMap<Integer, Ticket> tickets;
 
     public ATM (String email, String nombre){
         this.email=email;
         this.nombre=nombre;
+        tickets=new TreeMap<>();
     }
-    public void listTickets(int id){
 
+    /**
+     * The method runs through the tree and gives back the pair [K,V] ordered by the key
+     * @return The string returned is the list of tickets that belongs to the atm ordered by their id
+     */
+    public String listTickets(){
+        StringBuilder str= new StringBuilder();
+        str.append(toString());
+        for(Map.Entry<Integer, Ticket> it : tickets.entrySet()){//saca para cada nodo del arbol (K,V) ordenado por la clave
+            Integer id = it.getKey();
+            Ticket ticket = it.getValue();
+            str.append("Id del ticket: "+id+". Estado del ticket: "+ticket.getStatus()+"\n");
+        }
+        return str.toString();
     }
     public String toString(){
         return "Nombre del Cajero: "+nombre+". Email: "+email+"\n";
     }
-    public void createNewTicket(){
-
+    /**
+     * The method removes the ticket given by the id
+     * @param id the id of the ticket
+     * @return returns true if the ticket has been removed
+     */
+    public boolean removeTicket(int id){
+        boolean resul=false;
+        if(tickets.containsKey(id)){
+            resul=true;
+            tickets.remove(id);
+        }
+        return resul;
     }
-
-
 }
