8658d3cd694c0122b609b81273186b4b35508857
inaki <inakitaipeticse8@gmail,com>
2025-11-14 11:19:30 +0100

    hey

--- src/main/java/upm/etsisi/poo/es/Product.java
+++ src/main/java/upm/etsisi/poo/es/Product.java
@@ -1,67 +1,18 @@
 package upm.etsisi.poo.es;
 
-/*
-#Cada uno es un enum
-· Nuevos productos:         Comidas en campus; Reuniones
 
-#Metodos/atributos del enum
-· Poseen:                   fecha de caducidad, sin categoría y con número máximo de participantes (máximo 100) y cuyo precio es calculado por persona.
-                            Enumerado independiente, que no forme parte de type¿?
-
-#Metodos del enum: minimaPlanificacion
-· Creacion de los productos: Para las reuniones se requiere un tiempo mínimo de planificación de 12 horas
-                            mientras que para las comidas es necesario 3 días, por lo que, estos tiempos, deben respetarse
-                            a la hora de crear el producto y cerrar el ticket.
-
-#Metodos que pueden ser de implementacion, es decir, interfaz llamada: int numMaxTextosPersonalizados(Producto producto),
-#en donde, dependiendo del type of product, tendremos un numero maximo determinado. La interfaz vendra del que se encarga
-#de crear al producto.
-#Para la personalizacion, si maxPers es distinto a Integer.MaxValue es porque es personalizable.
-
-También se extienden los productos con una versión personalizable. Estos productos
-personalizados tienen una lista máxima de textos permitidos para el producto y el precio de
-estos es calculado agregándole al precio del producto sin personalizar un recargo del 10% por
-cada texto personalizado agregado. De esto productos se conoce el número máximo de textos
-personalizables por producto. No todos los productos básicos serán personalizables. Un
-producto básico no puede pasar a personalizable en el futuro.
-
-#Podemos hacer un metodo booleano que nos indica si el enumerado de FOOD o MEETING es usable o no, de modo que
-#en los metodos prod addFood y prodAddMeeting comprobemos esto para que este o no. En ese booleano tenemos el try-catch
-
-o prod add [<id>] "<name>" <category> <price> [<maxPers>]
-     si tiene <maxPers> se considerara que el producto es personalizable)
-o prod update <id> NAME|CATEGORY|PRICE <value>
-o prod addFood [<id>] "< name>" <price> <expiration: yyyy-MM-dd> <max_people>
-     El precio es por persona apuntada
-o prod addMeeting [<id>] "<name>" <price> < expiration: yyyy-MM-dd> <max_people >
-     El precio es por persona apuntada
-o prod list
-o prod remove <id>
- */
 
 enum type {
     MERCH, STATIONERY, CLOTHES, BOOK, ELECTRONICS
 }
 
-//#No necesitamos un metodo booleano extra, se hace la comprobacion desde prodAddFood o Meeting
-enum AdvanceProducts {
-    FOOD(72),
-    MEETING(12);
+//#Se hace la comprobacion desde prodAddFood o Meeting desde el getStateFood(...), pero como llevarlo a store¿?
+/*
+ATRIBUTOS/METODOS
+- fecha de caducidad, sin categoría y con número máximo de participantes (máximo 100) y cuyo precio es
+calculado por persona. Enumerado independiente, que no forme parte de type¿?
 
-    private final int advanceMinHours;
-
-    AdvanceProducts(int advanceMinHours){
-        this.advanceMinHours = advanceMinHours;
-    }
-
-    public int getAdvanceMinHours() {
-        return advanceMinHours;
-    }
-
-    public boolean getStateFood(int plannedHours){
-        return advanceMinHours>=plannedHours;
-    }
-}
+ */
 
 public class Product {
     private int id;
@@ -71,6 +22,40 @@ public class Product {
     private final static String NAME_LENGTH_ERROR="The name should be less than 100 characters";
     private final static String NAME_NULL_ERROR="The name shouldn't be empty";
     private final static String PRICE_POSITIVE_ERROR="WARNING: The price should be a positive number";
+
+    public enum AdvanceProducts{
+        FOOD(72),
+        MEETING(12);
+
+        private final int advanceMinHours;
+        private final int MAXPARTICIPANTES=100;
+        private String fechaCaducidad;
+        //precio de FOOD y MEETING es calculado por persona, metodo que calcula el precio/persona
+
+        AdvanceProducts(int advanceMinHours){
+            this.advanceMinHours = advanceMinHours;
+        }
+
+        public int getAdvanceMinHours() { //Se encarga del minimo timepo para que se pueda planificar tanto FOOD como MEETING
+            return advanceMinHours;
+        }
+        public int getMaxParticipantes(){
+            return MAXPARTICIPANTES;
+        }
+        public String getFechaCaducidad(){
+            int dia = (int)(Math.random()*(31-1+1))+1;
+            int mes = (int)(Math.random()*(12-1+1))+1;
+            int anho = (int)(Math.random()*(2030-2025+1))+2025;
+            this.fechaCaducidad = String.format("%02d/%02d/%d",dia, mes, anho);
+            return fechaCaducidad;
+        }
+        //public int getPrecio(){}
+
+        //Debe ser un metodo necesario a la hora de crear el producto y cerrar el ticket
+        public boolean getStateFood(int plannedHours){ //Devuelve un booleano para que se vea que este o no disponible para usarlo
+            return advanceMinHours>=plannedHours;
+        }
+    }
 
     public Product(int id, String name, type category, double price) {
         this.name = name.trim();
