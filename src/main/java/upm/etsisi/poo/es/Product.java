e370ee7c96c8ab7ab7a9e14eb19dda40a369e1c7
Miguel Alvarez Arevalo <miguel.aa0210@gmail.com>
2025-11-20 13:08:09 +0100

    erased unused classes and fixed compatibility between Product and Store

--- src/main/java/upm/etsisi/poo/es/Product.java
+++ src/main/java/upm/etsisi/poo/es/Product.java
@@ -1,15 +1,15 @@
 package upm.etsisi.poo.es;
+
 import java.time.LocalDateTime; //Importing for the methods of getStatusFood and getStatusMeeting
 
 public class Product {
     private int id;
     private String name;
     private double price;
-    private int maxPers;
     private type category;
-    private final static String NAME_LENGTH_ERROR="The name should be less than 100 characters";
-    private final static String NAME_NULL_ERROR="The name shouldn't be empty";
-    private final static String PRICE_POSITIVE_ERROR="WARNING: The price should be a positive number";
+    private final static String NAME_LENGTH_ERROR = "The name should be less than 100 characters";
+    private final static String NAME_NULL_ERROR = "The name shouldn't be empty";
+    private final static String PRICE_POSITIVE_ERROR = "WARNING: The price should be a positive number";
 
     public Product(int id, String name, type category, double price) {
         this.name = name.trim();
@@ -17,16 +17,10 @@ public class Product {
         this.id = id;
         this.category = category;
     }
-    public Product(int id, String name, type category, double price, int maxPers) {
-        this.name = name.trim();
-        this.price = price;
-        this.id = id;
-        this.category = category;
-        this.maxPers = maxPers;
-    }
+
 
     //Create a new constructor for doing the static class FOOD and MEETING for being called by other classes
-    public Product(int id, String name, double price){
+    public Product(int id, String name, double price) {
 
     }
 
@@ -112,28 +106,40 @@ public class Product {
     }
 
     //It's the operation of the price for customizable products
-    public double pricePerCustomizedText(){
-        return 0.1*price;
+    public double pricePerCustomizedText() {
+        return 0.1 * price;
     }
 
     //The three methods under that sentence are using to referencing the Food and Meeting methods
-    public boolean getStateFood(int numberParticipant){
+    public boolean getStateFood(int numberParticipant) {
         return true;
     }
-    public boolean getStateMeeting(int numberParticipant){
+
+    public boolean getStateMeeting(int numberParticipant) {
         return true;
     }
-    public String toStringFoodOrMeeting(){
+
+    public String toStringFoodOrMeeting() {
         return "";
     }
+
+    @Override
+/**
+ * the method turns the object into a String with the format required
+ */
+    public String toString() {
+        return "{class:Product, id: " + this.id + ", name: '" + this.name + "', category: " + this.category + ", price: " + this.price + "}";
+
+    }
+
 
     //This Food class can have its abstracted methods
     public static class Food extends Product {
         private final int advanceMinHours = 72;//It's in hours
-        private static final int MAXPARTICIPANTS=100;
+        private static final int MAXPARTICIPANTS = 100;
         private final String expiryDate;
 
-        public Food (int id, String name, double price, String expiryDate) {
+        public Food(int id, String name, double price, String expiryDate) {
             super(id, name, price);
             this.expiryDate = expiryDate;
         }
@@ -141,16 +147,17 @@ public class Product {
         public int getAdvanceMinHours() { //Se encarga del minimo timepo para que se pueda planificar tanto FOOD como MEETING
             return advanceMinHours;
         }
-        public int getMaxParticipants(){
+
+        public int getMaxParticipants() {
             return MAXPARTICIPANTS;
         }
 
-        public String getExpiryDate(){
+        public String getExpiryDate() {
             return expiryDate; //yyyy-MM-dd
         }
 
         //This method is necessary to create a product and closing the ticket
-        public boolean getStateFood(int numberParticipant){
+        public boolean getStateFood(int numberParticipant) {
             int numMaxParticipant = getMaxParticipants();
             String[] time = expiryDate.split("-"); //[yyyy, MM, dd]
             int yearOfExpire = Integer.parseInt(time[0]);
@@ -160,21 +167,22 @@ public class Product {
             int yearOfNow = rightNow.getYear();       // 2025
             int monthOfNow = rightNow.getMonthValue(); // 11 (Noviembre)
             int dayOfNow = rightNow.getDayOfMonth();
-            double plannedHours = ((yearOfExpire-yearOfNow)*12*31*24) + ((monthOfExpire-monthOfNow)*31*24) + ((daysOfExpire-dayOfNow)*24);
-            return advanceMinHours>=plannedHours && numberParticipant<=numMaxParticipant;
+            double plannedHours = ((yearOfExpire - yearOfNow) * 12 * 31 * 24) + ((monthOfExpire - monthOfNow) * 31 * 24) + ((daysOfExpire - dayOfNow) * 24);
+            return advanceMinHours >= plannedHours && numberParticipant <= numMaxParticipant;
         }
+
         public String toString() {
-            return super.toStringFoodOrMeeting() + "{class:Product, id: " + getId() + ", name: '" + getName() + ", price: " + getPrice() + ", expiry date: "+ expiryDate+" }";
+            return super.toStringFoodOrMeeting() + "{class:Product, id: " + getId() + ", name: '" + getName() + ", price: " + getPrice() + ", expiry date: " + expiryDate + " }";
         }
     }
 
     //This Meeting class can have its abstracted methods
     public static class Meeting extends Product {
         private final int advanceMinHours = 12;//It's in hours
-        private static final int MAXPARTICIPANTS=100;
+        private static final int MAXPARTICIPANTS = 100;
         private final String expiryDate;
 
-        public Meeting (int id, String name, double price, String expiryDate) {
+        public Meeting(int id, String name, double price, String expiryDate) {
             super(id, name, price);
             this.expiryDate = expiryDate;
         }
@@ -182,15 +190,17 @@ public class Product {
         public int getAdvanceMinHours() { //Se encarga del minimo timepo para que se pueda planificar tanto FOOD como MEETING
             return advanceMinHours;
         }
-        public int getMaxParticipants(){
+
+        public int getMaxParticipants() {
             return MAXPARTICIPANTS;
         }
-        public String getExpiryDate(){
+
+        public String getExpiryDate() {
             return expiryDate;
         }
 
         //This method is necessary to create a product and closing the ticket
-        public boolean getStateMeeting(int numberParticipant){
+        public boolean getStateMeeting(int numberParticipant) {
             int numMaxParticipant = getMaxParticipants();
             String[] time = expiryDate.split("-");
             int yearOfExpire = Integer.parseInt(time[0]);
@@ -200,21 +210,15 @@ public class Product {
             int yearOfNow = rightNow.getYear();
             int monthOfNow = rightNow.getMonthValue();
             int dayOfNow = rightNow.getDayOfMonth();
-            double plannedHours = ((yearOfExpire-yearOfNow)*12*31*24) + ((monthOfExpire-monthOfNow)*31*24) + ((daysOfExpire-dayOfNow)*24);
-            return advanceMinHours>=plannedHours && numberParticipant<=numMaxParticipant;
+            double plannedHours = ((yearOfExpire - yearOfNow) * 12 * 31 * 24) + ((monthOfExpire - monthOfNow) * 31 * 24) + ((daysOfExpire - dayOfNow) * 24);
+            return advanceMinHours >= plannedHours && numberParticipant <= numMaxParticipant;
         }
+
         public String toString() {
-            return super.toStringFoodOrMeeting() + "{class:Product, id: " + getId() + ", name: '" + getName() + ", price: " + getPrice() + ", expiry date: "+ expiryDate+" }";
+            return super.toStringFoodOrMeeting() + "{class:Product, id: " + getId() + ", name: '" + getName() + ", price: " + getPrice() + ", expiry date: " + expiryDate + " }";
         }
-    }
-
-
-    @Override
-    /**
-     * the method turns the object into a String with the format required
-     */
-    public String toString() {
-        return "{class:Product, id: " + this.id + ", name: '" + this.name + "', category: " + this.category + ", price: " + this.price + "}";
-
     }
 }
+
+
+
