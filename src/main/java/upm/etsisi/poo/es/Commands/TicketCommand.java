33544a30bfcea9c9cfa61958f439b805e64a6724
Miguel Alvarez Arevalo <miguel.aa0210@gmail.com>
2025-11-23 20:57:30 +0100

    fixed enum Status implementation in Ticket and boolean closed in TicketCommand

--- src/main/java/upm/etsisi/poo/es/Commands/TicketCommand.java
+++ src/main/java/upm/etsisi/poo/es/Commands/TicketCommand.java
@@ -6,83 +6,83 @@ import upm.etsisi.poo.es.Ticket;
 
 public class TicketCommand implements Command {
 
-    @Override
-    public String getName() {
-        return "ticket";
+  @Override
+  public String getName() {
+    return "ticket";
+  }
+
+  @Override
+  public String getDescription() {
+    return "ticket add|remove|print ...  - ticket management";
+  }
+
+  @Override
+  public boolean execute(String fullLine, String[] args, Store store, Ticket ticket) {
+    if (args.length < 2) {
+      System.out.println(INCORRECT);
+      return false;
     }
 
-    @Override
-    public String getDescription() {
-        return "ticket add|remove|print ...  - ticket management";
+    String sub = args[1];
+
+    switch (sub) {
+      case "add":
+        ticketAdd(args, store, ticket);
+        break;
+      case "remove":
+        ticketRemove(args, ticket);
+        break;
+      case "print":
+        ticketPrint(ticket);
+        break;
+      default:
+        System.out.println(INCORRECT);
     }
 
-    @Override
-    public boolean execute(String fullLine, String[] args, Store store, Ticket ticket) {
-        if (args.length < 2) {
-            System.out.println(INCORRECT);
-            return false;
-        }
+    return false;
+  }
 
-        String sub = args[1];
-
-        switch (sub) {
-            case "add":
-                ticketAdd(args, store, ticket);
-                break;
-            case "remove":
-                ticketRemove(args, ticket);
-                break;
-            case "print":
-                ticketPrint(ticket);
-                break;
-            default:
-                System.out.println(INCORRECT);
-        }
-
-        return false;
+  private void ticketAdd(String[] args, Store store, Ticket ticket) {
+    if (args.length != 4) {
+      System.out.println(INCORRECT);
+      return;
     }
-
-    private void ticketAdd(String[] args, Store store, Ticket ticket) {
-        if (args.length != 4) {
-            System.out.println(INCORRECT);
-            return;
-        }
-        try {
-            int id = Integer.parseInt(args[2]);
-            int amount = Integer.parseInt(args[3]);
-            ticket.ticketAdd(id, store, amount);
-            // ticketAdd ya imprime y dice "ticket add: ok" o errores
-        } catch (NumberFormatException e) {
-            System.out.println(INCORRECT);
-        }
+    try {
+      int id = Integer.parseInt(args[2]);
+      int amount = Integer.parseInt(args[3]);
+      ticket.ticketAdd(id, store, amount);
+      // ticketAdd ya imprime y dice "ticket add: ok" o errores
+    } catch (NumberFormatException e) {
+      System.out.println(INCORRECT);
     }
+  }
 
-    private void ticketRemove(String[] args, Ticket ticket) {
-        if (args.length != 3) {
-            System.out.println(INCORRECT);
-            return;
-        }
-        try {
-            int id = Integer.parseInt(args[2]);
-            Product product = ticket.ticketRemove(id);
-            if (product == null) {
-                System.out.println(NOTEXIST);
-            } else {
-                System.out.println(product.toString());
-                System.out.println("ticket remove: ok");
-            }
-        } catch (NumberFormatException e) {
-            System.out.println(INCORRECT);
-        }
+  private void ticketRemove(String[] args, Ticket ticket) {
+    if (args.length != 3) {
+      System.out.println(INCORRECT);
+      return;
     }
+    try {
+      int id = Integer.parseInt(args[2]);
+      Product product = ticket.ticketRemove(id);
+      if (product == null) {
+        System.out.println(NOTEXIST);
+      } else {
+        System.out.println(product.toString());
+        System.out.println("ticket remove: ok");
+      }
+    } catch (NumberFormatException e) {
+      System.out.println(INCORRECT);
+    }
+  }
 
-    private void ticketPrint(Ticket ticket) {
-        String printed = ticket.ticketPrint();
-        if (printed.isEmpty()) {
-            System.out.println(EMPTY_TICKET);
-        } else {
-            System.out.println(printed);
-            System.out.println("ticket print: ok");
-        }
+  private void ticketPrint(Ticket ticket) {
+    String printed = ticket.ticketPrint(true);
+    if (printed.isEmpty()) {
+      System.out.println(EMPTY_TICKET);
+    } else {
+      System.out.println(printed);
+      System.out.println("ticket print: ok");
     }
+  }
 }
--- src/main/java/upm/etsisi/poo/es/Ticket.java
+++ src/main/java/upm/etsisi/poo/es/Ticket.java
@@ -6,202 +6,201 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 enum Status {
-    EMPTY, ACTIVE, CLOSED;
+  EMPTY, ACTIVE, CLOSED;
 }
 
 public class Ticket {
-    final static int MAX_PRODUCT = 100;
-    Product[] productList;
-    int amount;
-    private StringBuilder id;
-    Comparator<Product> nameComp = Comparator.comparing(Product::getName);
-    private Status status;
+  final static int MAX_PRODUCT = 100;
+  Product[] productList;
+  int amount;
+  private StringBuilder id;
+  Comparator<Product> nameComp = Comparator.comparing(Product::getName);
+  private Status status;
 
-    public Ticket(int id) { //Aaron lo ha implementado con un int id en vez de una Store, ver cual es mejor
-        this.productList = new Product[MAX_PRODUCT];
-        LocalTime now = LocalTime.now();
-        this.id = new StringBuilder(now.toString()).append(String.format("%05d", id));
-        this.amount = 0;
-        this.status = Status.EMPTY;
-    }
+  public Ticket(int id) { // Aaron lo ha implementado con un int id en vez de una Store, ver cual es mejor
+    this.productList = new Product[MAX_PRODUCT];
+    LocalTime now = LocalTime.now();
+    this.id = new StringBuilder(now.toString()).append(String.format("%05d", id));
+    this.amount = 0;
+    this.status = Status.EMPTY;
+  }
 
+  public boolean ticketAdd(int proId, Store store, int amount) {
+    boolean resul;
 
-    public boolean ticketAdd(int proId, Store store, int amount) {
-        boolean resul;
+    if (this.status != Status.CLOSED) {
+      Product productoEncontrado = store.getProduct(proId);
+      int before = this.amount;
+      if (productoEncontrado == null) {
+        resul = false;
+        System.out.println("ERROR: Product ID not found " + proId);
 
-        if (this.status != Status.CLOSED) {
-            Product productoEncontrado = store.getProduct(proId);
-            int before = this.amount;
-            if (productoEncontrado == null) {
-                resul = false;
-                System.out.println("ERROR: Product ID not found " + proId);
-
-            } else {
-                if (this.amount == 0) {
-                    this.status = Status.ACTIVE;
-                }
-                int i = 0;
-                while (i < amount && this.amount < MAX_PRODUCT) {
-                    productList[this.amount] = productoEncontrado;
-                    this.amount++;
-                    i++;
-                }
-                System.out.println(ticketPrint(false));
-                if ((this.amount - before) == amount) {
-                    resul = true;
-                    System.out.println("ticket add: ok");
-                } else {
-                    resul = false;
-                    System.out.println("ERROR: Full Ticket (100 products max)");
-                }
-
-            }
+      } else {
+        if (this.amount == 0) {
+          this.status = Status.ACTIVE;
+        }
+        int i = 0;
+        while (i < amount && this.amount < MAX_PRODUCT) {
+          productList[this.amount] = productoEncontrado;
+          this.amount++;
+          i++;
+        }
+        System.out.println(ticketPrint(false));
+        if ((this.amount - before) == amount) {
+          resul = true;
+          System.out.println("ticket add: ok");
         } else {
-            resul = false;
-            System.out.println("ERROR: the ticket is closed. It can't be modified");
+          resul = false;
+          System.out.println("ERROR: Full Ticket (100 products max)");
         }
-        return resul;
+
+      }
+    } else {
+      resul = false;
+      System.out.println("ERROR: the ticket is closed. It can't be modified");
     }
+    return resul;
+  }
 
+  /**
+   * @param prodId This is Id from the product that sending us to remove
+   *               This method remove all occurrences of the product
+   * @return it's a boolean that checks if the product is removed
+   */
+  public Product ticketRemove(int prodId) {
+    Product product = null;
+    int iterations = this.amount;
+    if (this.status != Status.CLOSED) {
+      boolean removed = false;
+      if (this.amount == 0) {
+        System.out.println("ERROR: No products in the ticket");
 
-    /**
-     * @param prodId This is Id from the product that sending us to remove
-     *               This method remove all occurrences of the product
-     * @return it's a boolean that checks if the product is removed
-     */
-    public Product ticketRemove(int prodId) {
-        Product product = null;
-        int iterations = this.amount;
-        if (this.status != upm.etsisi.poo.es.status.CLOSED) {
-            boolean removed = false;
-            if (this.amount == 0) {
-                System.out.println("ERROR: No products in the ticket");
-
+      } else {
+        for (int i = 0; i < iterations; i++) {
+          if (productList[i] != null && productList[i].getId() == prodId) {
+            if (this.amount == 1) {
+              productList[0] = null;
+              this.amount--;
             } else {
-                for (int i = 0; i < iterations; i++) {
-                    if (productList[i] != null && productList[i].getId() == prodId) {
-                        if (this.amount == 1) {
-                            productList[0] = null;
-                            this.amount--;
-                        } else {
-                            product = productList[i];
-                            productList[i] = productList[amount - 1];
-                            productList[amount - 1] = null;
-                            this.amount--;
-                            i--;
-                        }
-                    }
-                }
-
-                boolean comprobation = true;
-                int i = 0;
-                while (comprobation && i < this.amount){
-                    if (productList[i] != null) {
-                        if (productList[i].getId() == prodId) {
-                            comprobation = false;
-                        }
-                    }
-                }
-                removed = comprobation;
-                sort();
-                if (iterations == this.amount){
-                    System.out.println("ERROR: this product does not exist.");
-                }
+              product = productList[i];
+              productList[i] = productList[amount - 1];
+              productList[amount - 1] = null;
+              this.amount--;
+              i--;
             }
-        }else{
-            System.out.println("ERROR: the ticket is closed. It can't be modified");
+          }
         }
-        return product;
 
-
-    }
-
-    /**
-     * @return the ticket printed
-     */
-    public String ticketPrint(boolean close) {
-        StringBuilder sc = new StringBuilder();
-        if ( close){
-            LocalDateTime now = LocalDateTime.now();
-            id.append(String.format(now.toString()));
-            this.status = upm.etsisi.poo.es.status.CLOSED;
-        }
-        System.out.println(id.toString());
-        if (this.amount > 0 && this.productList[0] != null) {
-            sort();
-            int n = this.amount;
-            int[] categoryCount = new int[type.values().length];
-            for (int i = 0; i < n; i++) {
-                Product p = productList[i];
-                if (p != null) {
-                    categoryCount[p.getCategory().ordinal()]++;
-                }
+        boolean comprobation = true;
+        int i = 0;
+        while (comprobation && i < this.amount) {
+          if (productList[i] != null) {
+            if (productList[i].getId() == prodId) {
+              comprobation = false;
             }
-
-            double totalPrice = 0.0;
-            double totalDiscount = 0.0;
-
-            for (int i = 0; i < n; i++) {
-                Product p = productList[i];
-
-                if (p != null) {
-                    double price = p.getPrice();
-                    double discountValue = 0.0;
-
-                    if (categoryCount[p.getCategory().ordinal()] >= 2) {
-                        discountValue = price - p.getDiscountedPrice();
-                    }
-
-                    totalPrice += price;
-                    totalDiscount += discountValue;
-
-                    if (discountValue > 0.0) {
-                        sc.append(String.format(
-                                "{class:Product, id: %d, name: '%s', category: %s, price: %.2f} **discount -%.2f", p.getId(), p.getName(), p.getCategory(), price, discountValue));
-                    } else {
-                        sc.append(String.format(
-                                "{class:Product, id: %d, name: '%s', category: %s, price: %.2f}", p.getId(), p.getName(), p.getCategory(), price));
-                    }
-                    sc.append("\n");
-                }
-            }
-
-            double finalPrice = totalPrice - totalDiscount;
-            sc.append("Total price: ").append(String.format("%.2f", totalPrice));
-            sc.append("\nTotal discount: ").append(String.format("%.2f", totalDiscount));
-            sc.append("\nFinal price: ").append(String.format("%.2f", finalPrice));
+          }
         }
-
-        return sc.toString();
-    }
-
-    public status setStatus(String status) {
-        return this.status = upm.etsisi.poo.es.status.valueOf(status);
-    }
-
-    /**
-     * The method sorts the names alphabetically
-     */
-    public void sort() {
-        Arrays.sort(productList, 0, amount, nameComp);
-    }
-
-    public String getStatus(){
-        String str;
-        switch (this.status){
-            case EMPTY:
-                str="Empty";
-                break;
-            case ACTIVE:
-                str="Active";
-            case CLOSED:
-                str="Closed";
-                break;
-            default:
-                str="Error, status is undefined";
-                break;
+        removed = comprobation;
+        sort();
+        if (iterations == this.amount) {
+          System.out.println("ERROR: this product does not exist.");
         }
-        return str;
+      }
+    } else {
+      System.out.println("ERROR: the ticket is closed. It can't be modified");
     }
+    return product;
+
+  }
+
+  /**
+   * @return the ticket printed
+   */
+  public String ticketPrint(boolean close) {
+    StringBuilder sc = new StringBuilder();
+    if (close) {
+      LocalDateTime now = LocalDateTime.now();
+      id.append(String.format(now.toString()));
+      this.status = Status.CLOSED;
+    }
+    System.out.println(id.toString());
+    if (this.amount > 0 && this.productList[0] != null) {
+      sort();
+      int n = this.amount;
+      int[] categoryCount = new int[type.values().length];
+      for (int i = 0; i < n; i++) {
+        Product p = productList[i];
+        if (p != null) {
+          categoryCount[p.getCategory().ordinal()]++;
+        }
+      }
+
+      double totalPrice = 0.0;
+      double totalDiscount = 0.0;
+
+      for (int i = 0; i < n; i++) {
+        Product p = productList[i];
+
+        if (p != null) {
+          double price = p.getPrice();
+          double discountValue = 0.0;
+
+          if (categoryCount[p.getCategory().ordinal()] >= 2) {
+            discountValue = price - p.getDiscountedPrice();
+          }
+
+          totalPrice += price;
+          totalDiscount += discountValue;
+
+          if (discountValue > 0.0) {
+            sc.append(String.format(
+                "{class:Product, id: %d, name: '%s', category: %s, price: %.2f} **discount -%.2f", p.getId(),
+                p.getName(), p.getCategory(), price, discountValue));
+          } else {
+            sc.append(String.format(
+                "{class:Product, id: %d, name: '%s', category: %s, price: %.2f}", p.getId(), p.getName(),
+                p.getCategory(), price));
+          }
+          sc.append("\n");
+        }
+      }
+
+      double finalPrice = totalPrice - totalDiscount;
+      sc.append("Total price: ").append(String.format("%.2f", totalPrice));
+      sc.append("\nTotal discount: ").append(String.format("%.2f", totalDiscount));
+      sc.append("\nFinal price: ").append(String.format("%.2f", finalPrice));
+    }
+
+    return sc.toString();
+  }
+
+  public Status setStatus(String status) {
+    return this.status = Status.valueOf(status);
+  }
+
+  /**
+   * The method sorts the names alphabetically
+   */
+  public void sort() {
+    Arrays.sort(productList, 0, amount, nameComp);
+  }
+
+  public String getStatus() {
+    String str;
+    switch (this.status) {
+      case EMPTY:
+        str = "Empty";
+        break;
+      case ACTIVE:
+        str = "Active";
+      case CLOSED:
+        str = "Closed";
+        break;
+      default:
+        str = "Error, status is undefined";
+        break;
+    }
+    return str;
+  }
 
 }
