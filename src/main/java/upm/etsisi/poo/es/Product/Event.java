bdf02c41fabf10e47c4bbe8181b8674cbf8caaeb
Miguel Alvarez Arevalo <miguel.aa0210@gmail.com>
2025-11-28 08:16:51 +0100

    added toString methods to products

--- src/main/java/upm/etsisi/poo/es/Product/Event.java
+++ src/main/java/upm/etsisi/poo/es/Product/Event.java
@@ -4,15 +4,15 @@ import upm.etsisi.poo.es.Commands.ProductCommand;
 
 public class Event extends Product {
 
-    private int maxPersonas;
-    private int personasActuales;
-    private String expiracyDate;
+    protected int maxPersonas;
+    protected  int personasActuales;
+    protected  String expiracyDate;
 
-    public Event(int id, String name, double price, String expiryDate, int maxPersonas) {
+    public Event(int id, String name, double price, String expiracyDate, int maxPersonas) {
         this.id = id;
         this.name = name;
         this.price = price;
-        this.expiracyDate = expiryDate;
+        this.expiracyDate = expiracyDate;
         this.maxPersonas = maxPersonas;
         this.personasActuales = 0;
     }
@@ -35,5 +35,10 @@ public class Event extends Product {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String toString(){
+        return "Class:Food, id:" + this.id + ", name:" + this.name + ", price:" + this.price + ", date of Event:" + this.expiracyDate +
+                ", max people allowed:" + this.maxPersonas;
     }
 }
