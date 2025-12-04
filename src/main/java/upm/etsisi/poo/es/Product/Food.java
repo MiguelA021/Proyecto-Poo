069e0d5c57c39f9233d4242594430ff00ef1228b
Aaron Apunte <aaronapuntesierra@gmail.com>
2025-12-04 17:04:43 +0100

    Arreglado para que compruebe en días y no horas fechaValida de Food

--- src/main/java/upm/etsisi/poo/es/Product/Food.java
+++ src/main/java/upm/etsisi/poo/es/Product/Food.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
 public class Food extends Event {
-    private final static int TIEMPO_MINIMO = 3; // horas
+    private final static int TIEMPO_MINIMO = 3; // días
 
     public Food(int id, String name, double price, String expiracyDate) {
         super(id, name, price, expiracyDate);
@@ -18,7 +18,7 @@ public class Food extends Event {
         }
 
         // Horas que faltan hasta el evento (evento - ahora)
-        long horas = ChronoUnit.HOURS.between(time, this.getExpiryDate());
+        long horas = ChronoUnit.DAYS.between(time, this.getExpiryDate());
 
         // Válido si faltan al menos TIEMPO_MINIMO horas
         return horas >= TIEMPO_MINIMO;
