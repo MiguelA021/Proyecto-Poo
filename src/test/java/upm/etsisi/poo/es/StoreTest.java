fda9c2fddab7e0eb9e4d02a20ea065b54c58857c
inaki <inakitaipeticse8@gmail,com>
2025-12-24 15:24:20 +0100

    DOING THE TEST FROM STORE IN StoreTest.java. I've done the middle of test.

--- src/test/java/upm/etsisi/poo/es/StoreTest.java
+++ src/test/java/upm/etsisi/poo/es/StoreTest.java
@@ -2,15 +2,193 @@ package upm.etsisi.poo.es;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
-import upm.etsisi.poo.es.Product.BasicProduct;
-import upm.etsisi.poo.es.Product.Product;
+import upm.etsisi.poo.es.Product.*;
+import upm.etsisi.poo.es.User.*;
 import upm.etsisi.poo.es.Store.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class StoreTest {
 
-    //***Test de que no permite hacer PRODADD cuando este lleno
+    //TEST QUE ME DEVUELVA UN PRODUCTO ENTRE LOS QUE ESTAN AÑADIDOS (3 PRODUCTOS AÑADIDOS)
     @Test
     public void getProductTest(){
+        Store objectTest = new Store();
+        Product productTest = new BasicProduct(1, "Libro BBDD", type.BOOK, 25);
+        Product productTest2 = new BasicProduct(2, "Sudadera tala:S UPM", type.CLOTHES, 15);
+        Product productTest3 = new BasicProduct(3, "Iphone UPM", type.ELECTRONICS, 200);
+        Product productTest4 = new BasicProduct(2, "Sudadera tala:S UPM", type.CLOTHES, 15);
+        objectTest.prodAdd(productTest);
+        objectTest.prodAdd(productTest2);
+        objectTest.prodAdd(productTest3);
+        objectTest.prodAdd(productTest4);
+        Product productResult = objectTest.getProduct(2);
+        assertEquals(productTest2, productResult);
+    }
+
+    //public boolean addFood(int id, String name, int price, String expiryDate, int assistants)
+    //SE USA CON GET PRODUCT PERO TIENE SU TEST SIMPLE de aumento de size 23458 "Cafeteria ETSISI" 5 2025-12-21 300
+
+    //FALLO: NO ADMITE EL MAX PERSONAS EN CONSTRUCTOR DE FOOD
+    @Test
+    public void addFoodTest(){
+        Store objectTest = new Store();
+        Product productTest = new Food(23458, "Cafeteria ETSISI", 5, "2025-12-21");
+        objectTest.prodAdd(productTest);
+        assertEquals(1, objectTest.getProdAmount());
+    }
+
+    //public boolean addMeeting(int id, String name, double price, String expiryDate, int assistants)
+    //SE USA CON GET PRODUCT PERO TIENE SU TEST SIMPLE 23457 "Graduacion ETSISI" 40 2025-12-21 30
+
+    //FALLO: NO ADMITE EL MAX PERSONAS EN CONSTRUCTOR DE MEETING
+    @Test
+    public void addMeetingTest(){
+        Store objectTest = new Store();
+        Product productTest = new Meeting(23457, "Graduacion ETSISI", 40, "2025-12-21");
+        objectTest.prodAdd(productTest);
+        assertEquals(1, objectTest.getProdAmount());
+    }
+
+    //public void addCustomer(String name, String dni, String email, int cashId)
+    //SE USA CON REMOVE CUSTOMER Y ARRAYLIST PERO TIENE SU TEST SIMPLE "Pepe3" 55630667S pepe1@upm.es U1234567
+    // UW1234567 "pepecurro3" pepe0@upm.es
+
+    //FALLO: preferible que el CASHIERID sea STRING
+    @Test
+    public void addCustomerEnterpriseTest(){
+        Store objectTest = new Store();
+        objectTest.addCasher(1234567, "AndresCurro5", "andresCurr@upm.es");
+        objectTest.addCustomer("Pepe3", "55630667S", "pepe1@upm.es", 1234567);
+        objectTest.addCustomer("Marco", "55630668J", "marcoo@upm.es", 1234567);
+        objectTest.addCustomer("Darkiel", "34670161A", "darkiel2@upm.es", 1234567);
+        assertEquals(3, objectTest.customers.size());
+    }
+
+    //Test de ADDCUSTOMER PERO QUE NO SEA ENTERPRISE
+    @Test
+    public void addCustomerNotEnterpriseTest(){
+        Store objectTest = new Store();
+        objectTest.addCasher(1234567, "AndresCurro5", "andresCurr@upm.es");
+        objectTest.addCustomer("Pepe3", "926306770", "pepe1@upm.es", 1234567);
+        objectTest.addCustomer("Marco", "516706681", "marcoo@upm.es", 1234567);
+        objectTest.addCustomer("Darkiel", "346701612", "darkiel2@upm.es", 1234567);
+        assertEquals(3, objectTest.customers.size());
+    }
+
+    //TEST DE ADD CUSTOMER QUE NO PERMITA UN DNI DE DIGITOS MENORES A 9 EN CASO DE NIF
+
+    //CORREGIR CODIGO EN STORE PARA QUE ESTE TEST SALGA BIEN
+    @Test
+    public void addCorrectCustomerNotEnterpriseTest(){
+        Store objectTest = new Store();
+        objectTest.addCasher(1234567, "AndresCurro5", "andresCurr@upm.es");
+        objectTest.addCustomer("Pepe3", "9263067", "pepe1@upm.es", 1234567);
+        objectTest.addCustomer("Marco", "51670661", "marcoo@upm.es", 1234567);
+        objectTest.addCustomer("Darkiel", "346701612", "darkiel2@upm.es", 1234567);
+        assertEquals(1, objectTest.customers.size());
+    }
+
+    //public Cashier searchCasherById(int id)
+    //TEST QUE ME COMPRUEBA QUE HAYA UN CAJERO ENTRE LOS 3 QUE SE HA AÑADIDO
+    @Test
+    public void searchCasherByIdTest(){
+        Store objectTest = new Store();
+        int id = 1234567;
+        objectTest.addCasher(1234567, "AndresCurro5", "andresCurr@upm.es");
+        objectTest.addCasher(2135597, "AndresCurro7", "andresCu77@upm.es");
+        objectTest.addCasher(7226967, "AndresCurro9", "andresCu99@upm.es");
+        assertEquals(objectTest.cashers.get(id), objectTest.searchCasherById(id));
+    }
+
+    //public int dniToId(String dni)
+    //TEST QUE ME COMPRUEBA LA CONVERSION
+
+    //FALLO: Al hacer id += c hace la suma del CHAR en ASCII, hay que convertir c en un int: id += (c - '0')
+    @Test
+    public void dniToIdTest(){
+        Store objectTest = new Store();
+        String dniTest = "55630667S";
+        assertEquals(55630667, objectTest.dniToId(dniTest));
+    }
+
+    //public boolean removeCustomer(String dni)
+    //TEST QUE ME COMPRUEBA LA ELIMINACION DE UN CLIENTE ENTRE LOS 3 QUE SE AÑADIO
+
+    @Test
+    public void removeCustomerTest(){
+        Store objectTest = new Store();
+        objectTest.addCasher(1234567, "AndresCurro5", "andresCurr@upm.es");
+        objectTest.addCustomer("Pepe3", "55630667S", "pepe1@upm.es", 1234567);
+        objectTest.addCustomer("Marco", "55630668J", "marcoo@upm.es", 1234567);
+        objectTest.addCustomer("Darkiel", "34670161A", "darkiel2@upm.es", 1234567);
+        objectTest.removeCustomer("55630667S");
+        assertEquals(2, objectTest.customers.size());
+    }
+
+    //public void listCustomers()
+    //	TEST QUE ME COMPRUEBA EL FORMATO CON DOS CLIENTES AÑADIDOS A TRAVES DE STRINGBUILDER RESULTADO Y ESPERADO
+
+    //FALLO: Debe ser USER en vez de CLIENT
+    @Test
+    public void listCustomersTest(){
+        PrintStream originalOut = new PrintStream(System.out);
+        ByteArrayOutputStream capturador = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(capturador));
+        Store objectTest = new Store();
+
+        objectTest.addCasher(1234567, "AndresCurro5", "andresCurr@upm.es");
+        objectTest.addCustomer("Pepe3", "55630667S", "pepe1@upm.es", 1234567);
+        objectTest.addCustomer("Marco", "55630668J", "marcoo@upm.es", 1234567);
+        objectTest.addCustomer("Darkiel", "34670161A", "darkiel2@upm.es", 1234567);
+        objectTest.listCustomers();
+
+        String resultFunction = capturador.toString();
+
+        StringBuilder sc = new StringBuilder();
+        sc.append("Cash{identifier='UW1234567', name='AndresCurro5', email='andresCurr@upm.es'}").append(System.lineSeparator());
+        sc.append("cash add: ok").append(System.lineSeparator());
+        sc.append("Client{identifier='55630667S', name='Pepe3', email='pepe1@upm.es', cash=UW1234567}").append(System.lineSeparator());
+        sc.append("client add: ok").append(System.lineSeparator());
+        sc.append("Client{identifier='55630668J', name='Marco', email='marcoo@upm.es', cash=UW1234567}").append(System.lineSeparator());
+        sc.append("client add: ok").append(System.lineSeparator());
+        sc.append("Client{identifier='34670161A', name='Darkiel', email='darkiel2@upm.es', cash=UW1234567}").append(System.lineSeparator());
+        sc.append("client add: ok").append(System.lineSeparator());
+        sc.append("Client: ").append(System.lineSeparator());
+        sc.append("  USER{identifier='34670161A', name='Darkiel', email='darkiel2@upm.es', cash=UW1234567}").append(System.lineSeparator());
+        sc.append("  USER{identifier='55630668J', name='Marco', email='marcoo@upm.es', cash=UW1234567}").append(System.lineSeparator());
+        sc.append("  USER{identifier='55630667S', name='Pepe3', email='pepe1@upm.es', cash=UW1234567}").append(System.lineSeparator());
+        sc.append("client list: ok").append(System.lineSeparator());
+        String salidaEsperada = sc.toString();
+
+        assertEquals(salidaEsperada, resultFunction);
+        System.setOut(originalOut);
+    }
+
+   // public void addTicketOnCashier(Integer idTicket, int idCashier, int idCustomer)
+    // TEST QUE ME COMPRUEBA SI UN TICKET PERTENECE A UN CAJERO CON EL DICCIONARIO DE STORE A PARTIR DEL ID DEL CAJERO Y ID DEL CLIENTE
+
+    @Test
+    public void addTicketOnCashierTest(){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Test de que no permite hacer PRODADD cuando este lleno
+    @Test
+    public void prodAddOnMaxSizeTest(){
         Store objeto = new Store();
         int capacidadMaxima = objeto.getMAX_PRODUCT();
         for (int i = 0; i < capacidadMaxima; i++) {
@@ -21,13 +199,56 @@ public class StoreTest {
         assertFalse("El array de ProductList esta lleno", result);
     }
 
-    //TEST QUE ME DEVUELVA UN PRODUCTO CON GETPRODUCT
-
-
-
     //TEST QUE ME VERIFIQUE QUE AUMENTA EL TAMAÑO DE PRODUCTLIST
-
-
-
+    @Test
+    public void prodAddSizeTest(){
+        Store objectTest = new Store();
+        Product productTest = new BasicProduct(1, "Libro BBDD", type.BOOK, 25);
+        int size = 0;
+        objectTest.prodAdd(productTest);
+        assertEquals(objectTest.getProdAmount(), size+1);
+    }
 
 }
+
+/*
+public boolean addCasher(Integer id, String name, String email)
+	SE USA CON REMOVE/LIST DE CASHIER PERO TIENE SU TEST SIMPLE
+
+public boolean removeCasher(int id)
+	TEST QUE ME COMPRUEBA QUE ESTAN TODOS LOS CAJEROS MENOS EL ELIMINADO COMPARANDO LOS ARRAY LIST DE CAJEROS ESPERADOS CON EL ASSERT STRING DEL LISTCASHIER
+
+public void listCashers()
+	TEST QUE ME COMPRUEBA CON  EL ASSERT DE STRING EL MISMO STRING DE UNO CON LOS CAJEROS AÑADIDOS RESULTADO DE LA FUNCION Y OTRO PROPIO NUESTRO, USAMOS UN LIST CASHERS DE 3
+
+private ArrayList<Cashier> cashersToList()
+	SE USA PARA LIST CASHERS/ REMOVE CASHERS Y ADD CASHERS
+
+
+
+
+public void listTicketsOnCasher(int id)
+	TEST PARECIDO AL LIST CASHERS Y LIST CUSTOMERS
+
+public boolean prodRemove(int id)
+	TEST QUE ME COMPRUEBA LA ELIMINACION DE UN PRODUCTO TRAS 3 QUE HABIAN
+
+public void prodList()
+	TEST QUE ME COMPRUEBA FORMATO, TANTO EL QUE USA ESTA FUNCION COMO EL STRINGBUILDER PROPIO
+
+public Product updateType(int id, type category)
+	TEST QUE ME COMRUEBA EL CAMBIO DE TIPO
+
+public Product updateName(int id, String name)
+	TEST QUE ME COMRUEBA EL CAMBIO DE NOMBRE
+
+public Product updatePrice(int id, double price)
+	TEST QUE ME COMRUEBA EL CAMBIO DE PRECIO
+
+public void ticketList()
+	LO MISMO QUE CON LAS OTRAS COMPARACIONES DE FORMATO
+
+public Cashier getCasher(int cashId)
+	PARA COMPROBAR EL CAJERO COMO EN GET PRODUCT CON 5 CAJEROS
+
+ */
