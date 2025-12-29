9ef4295e5285eefb80b1a306175b4de9e5184b6b
Miguel Alvarez Arevalo <miguel.aa0210@gmail.com>
2025-12-29 18:55:35 +0100

    erased unused store

--- src/main/java/upm/etsisi/poo/es/Commands/TicketCommand.java
+++ src/main/java/upm/etsisi/poo/es/Commands/TicketCommand.java
@@ -30,7 +30,6 @@ public class TicketCommand implements Command {
 
   @Override
   public boolean execute(String fullLine, String[] args) {
-      Store store = Store.getInstance();
     if (args.length < 2) {
       System.out.println(INCORRECT);
       return false;
