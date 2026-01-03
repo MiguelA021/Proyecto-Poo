c292d213b40578a4955ed5c1cd02565b3df135c4
Aaron Apunte <aaronapuntesierra@gmail.com>
2026-01-03 15:12:00 +0100

    Cambios para clientes

--- src/main/java/upm/etsisi/poo/es/User/Customer.java
+++ src/main/java/upm/etsisi/poo/es/User/Customer.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 
 public class Customer extends User {
   protected final int cashierId;
-
+  private ArrayList<Integer> tickets ;
   public Customer(String email, String name, String id, int cashierId) {
 
     this.email = email;
     this.name = name;
     this.cashierId = cashierId;
     this.id = id;
-    tickets = new ArrayList<Integer>();
+    this.tickets = new ArrayList<Integer>();
   }
 
   public String toString() {
@@ -24,7 +24,7 @@ public class Customer extends User {
    * @param id     the id of the ticket
    */
   public void addTicket(Integer id) {
-    tickets.add(id);
+    this.tickets.add(id);
   }
 
 }
--- src/main/java/upm/etsisi/poo/es/User/CustomerEnterprise.java
+++ src/main/java/upm/etsisi/poo/es/User/CustomerEnterprise.java
@@ -1,12 +1,25 @@
 package upm.etsisi.poo.es.User;
 
-public class CustomerEnterprise extends Customer{
+import java.util.ArrayList;
+
+public class CustomerEnterprise extends Customer {
+    private ArrayList<Integer> tickets;
+    private final int cashierId;
+
     public CustomerEnterprise(String email, String name, String id, int cashierId) {
         super(email, name, id, cashierId);
+        this.email = email;
+        this.name = name;
+        this.cashierId = cashierId;
+        this.id = id;
+        this.tickets = new ArrayList<Integer>();
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return "COMPANY{identifier='" + id + "', name='" + name + "', email='" + email + "', cash=UW" + cashierId + "}";
     }
+    @Override
+    public void addTicket(Integer id) {this.tickets.add(id);}
+
 }
