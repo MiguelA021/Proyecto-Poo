7ff6d84942038c7bfbf18c9a67c4daa788539a10
Miguel Alvarez Arevalo <miguel.aa0210@gmail.com>
2026-01-04 21:53:46 +0100

    finished products

--- src/main/java/upm/etsisi/poo/es/Product/PersonalizedProduct.java
+++ src/main/java/upm/etsisi/poo/es/Product/PersonalizedProduct.java
@@ -2,17 +2,25 @@ package upm.etsisi.poo.es.Product;
 
 import upm.etsisi.poo.es.type;
 
-import java.util.ArrayList;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Locale;
+import java.util.Set;
 
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class PersonalizedProduct extends BasicProduct {
+    @Column(name = "maxPersonalizaciones")
     private int maxPers;
-    private ArrayList<String> personalizaciones;
+
+    @OneToMany(mappedBy = "id", cascade = CascadeType.ALL)
+    private Set<Personalization> personalizaciones;
 
     public PersonalizedProduct(int id, String name, type type, double price, int maxPers) {
         super(id, name, type, price);
         this.maxPers = maxPers;
-        this.personalizaciones = new ArrayList<String>(maxPers);
+        this.personalizaciones = new HashSet<Personalization>(maxPers);
     }
 
     public int getMaxPers() {
@@ -25,10 +33,12 @@ public class PersonalizedProduct extends BasicProduct {
         if (!this.personalizaciones.isEmpty()) {
             sb.append("{class:ProductPersonalized, id:").append(this.id).append(", name:'").append(this.name).append("', category: ").append(this.category).append(", price:").append(this.price).append(",");
             sb.append(" maxPersonal:").append(maxPers).append(",\n");
-            for (int i = 0; i < this.personalizaciones.size(); i++) {
-                if (i == this.personalizaciones.size() - 1) {
-                    sb.append(personalizaciones.get(i));
-                } else sb.append(personalizaciones.get(i)).append("\t");
+            Iterator it = personalizaciones.iterator();
+            if(it.hasNext()){
+                sb.append(it.next());
+            }
+            while (it.hasNext()){
+                sb.append(", "+it.next());
             }
         } else {
             sb.append("{class:ProductPersonalized, id:").append(this.id).append(", name:'").append(this.name).append("', category: ").append(this.category).append(", price:").append(this.price);
@@ -47,7 +57,8 @@ public class PersonalizedProduct extends BasicProduct {
     public boolean addPersonalized(String personalize) {
         boolean resul = true;
         if (personalizaciones.size() < maxPers) {
-            personalizaciones.add(personalize);
+            Personalization personalization = new Personalization(personalize);
+            personalizaciones.add(personalization);
         } else {
             resul = false;
         }
@@ -69,10 +80,12 @@ public class PersonalizedProduct extends BasicProduct {
             sb.append(" maxPersonal:").append(maxPers).append(",");
             sb.append("personalizationList:[");
 
-            for (int i = 0; i < this.personalizaciones.size(); i++) {
-                if (i == this.personalizaciones.size() - 1) sb.append(personalizaciones.get(i));
-                else sb.append(personalizaciones.get(i)).append(", ");
-
+            Iterator it = personalizaciones.iterator();
+            if(it.hasNext()){
+                sb.append(it.next());
+            }
+            while (it.hasNext()){
+                sb.append(", "+it.next());
             }
             sb.append("]} ");
         } else {
